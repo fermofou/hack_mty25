b75897fe30235b515e497940fe5cfe8dc23711be
fermofou <fermofou@gmail.com>
2025-10-25 12:39:18 -0600

    addded admin

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,17 @@
-import { BrowserRouter, Route, Routes } from 'react-router';
-import { AuthProvider } from './context/AuthProvider';
-import LoginPage from './pages/LoginPage';
-import UserDashboard from './pages/UserDashboard';
+import { BrowserRouter, Route, Routes } from "react-router";
+import { AuthProvider } from "./context/AuthProvider";
+import LoginPage from "./pages/LoginPage";
+import UserDashboard from "./pages/UserDashboard";
+import AdminPage from "./pages/AdminPage";
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
         <Routes>
-          <Route path='/' element={<LoginPage />} />
-          <Route path='/user/dashboard' element={<UserDashboard />} />
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/user/dashboard" element={<UserDashboard />} />
+          <Route path="/admin" element={<AdminPage />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
